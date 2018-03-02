25d4bb317b1d17e9b7307715adf7430db00a8b48
Burt Beckwith <burt@burtbeckwith.com>
2018-03-02 06:48:44 -0500

    cleanup of BuildConfig.groovy updated to servlet version 3.0 removed unnecessary antlr and javassist dependencies (they're dependencies of the hibernate plugin) removed unnecessary spring-security-config, spring-security-web, and junit dependencies removed servlet-api dependency and the deletion from the war removed unused rest-client-builder and release plugin dependencies updated spring-security-core and spring-security-ldap versions from RC5 to 2.0.0 removed -SNAPSHOT from transmart plugin dependency versions un-commented transitive transmart plugin dependencies for clarity

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,206 +1,146 @@
-def forkSettingsRun = [
-        minMemory: 1536,
-        maxMemory: 4096,
-        maxPerm  : 384,
-        debug    : false,
-]
-def forkSettingsOther = [
-        minMemory: 256,
-        maxMemory: 1024,
-        maxPerm  : 384,
-        debug    : false,
-]
+import grails.util.Environment
 
-grails.project.fork = [
-        test   : forkSettingsOther,
-        run    : forkSettingsRun,
-        war    : false,
-        console: forkSettingsOther]
+def forkSettingsRun   = [minMemory: 1536, maxMemory: 4096, maxPerm: 384, debug: false]
+def forkSettingsOther = [minMemory:  256, maxMemory: 1024, maxPerm: 384, debug: false]
 
-grails.project.war.file = "target/${appName}.war"
-
-/* we need at least servlet-api 2.4 because of HttpServletResponse::setCharacterEncoding */
-grails.servlet.version = "2.5"
+def dm
+try {
+	Class dmClass = new GroovyClassLoader().parseClass(
+			new File('../transmart-dev/DependencyManagement.groovy'))
+	dm = dmClass?.newInstance()
+}
+catch (ignored) {}
 
 grails.project.dependency.resolver = 'maven'
-
-def dm, dmClass
-try {
-    dmClass = new GroovyClassLoader().parseClass(
-            new File('../transmart-dev/DependencyManagement.groovy'))
-} catch (Exception e) {
-}
-if (dmClass) {
-    dm = dmClass.newInstance()
-}
+grails.project.fork = [test: forkSettingsOther, run: forkSettingsRun, war: false, console: forkSettingsOther]
+grails.project.war.file = "target/${appName}.war"
+grails.project.work.dir = 'target'
+grails.servlet.version = '3.0'
 
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
-    inherits("global") {}
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+	inherits 'global'
+	log 'warn'
+	checksums true
+	legacyResolve false
 
-    if (!dm) {
-        repositories {
-            grailsCentral()
-            mavenLocal()
-            mavenCentral()
+	if (!dm) {
+		repositories {
+			mavenLocal()
+			grailsCentral()
+			mavenCentral()
+			mavenRepo 'https://repo.transmartfoundation.org/content/repositories/public/'
+		}
+	}
+	else {
+		dm.configureRepositories delegate
+	}
 
-            mavenRepo "https://repo.transmartfoundation.org/content/repositories/public/"
-        }
-    } else {
-        dm.configureRepositories delegate
-    }
+	dependencies {
+		compile 'axis:axis:1.4' // for GeneGo web services
+		compile 'com.google.guava:guava:18.0'
+		compile 'commons-net:commons-net:3.3' // used for ftp transfers
+		compile 'net.sf.ehcache:ehcache:2.9.0'
+		compile 'net.sf.opencsv:opencsv:2.3'
+		compile 'org.apache.commons:commons-math:2.2' //>2MB lib briefly used in ChartController
+		compile 'org.apache.httpcomponents:httpclient:4.4.1'
+		compile 'org.apache.httpcomponents:httpcore:4.4.1'
+		compile 'org.apache.lucene:lucene-core:2.4.0'
+		compile 'org.apache.lucene:lucene-demos:2.4.0'
+		compile 'org.apache.lucene:lucene-highlighter:2.4.0'
+		compile 'org.apache.solr:solr-core:5.4.1'
+		compile 'org.apache.solr:solr-solrj:5.4.1'
+		compile 'org.codehaus.groovy.modules.http-builder:http-builder:0.5.2', {
+			excludes 'groovy', 'nekohtml', 'httpclient', 'httpcore'
+		}
+		compile 'org.rosuda:Rserve:1.7.3'
+		compile('org.springframework.security.extensions:spring-security-saml2-core:1.0.0.RELEASE') {
+			excludes 'spring-security-config', 'spring-security-core', 'spring-security-web', 'xercesImpl'
+		}
+		compile 'org.transmartproject:transmart-core-api:16.2'
 
-    dependencies {
-        // you can remove whichever you're not using
-        runtime 'org.postgresql:postgresql:9.3-1100-jdbc4'
-        runtime 'com.oracle:ojdbc7:12.1.0.1'
+		// you can remove whichever you're not using
+		runtime 'org.postgresql:postgresql:9.3-1100-jdbc4'
+		runtime 'com.oracle:ojdbc7:12.1.0.1'
 
-        runtime 'org.javassist:javassist:3.16.1-GA'
+		test 'org.gmock:gmock:0.9.0-r435-hyve2', { transitive = false }
+		test 'org.hamcrest:hamcrest-core:1.3'
+		test 'org.hamcrest:hamcrest-library:1.3'
+	}
 
-        compile 'org.transmartproject:transmart-core-api:16.2-SNAPSHOT'
+	plugins {
+		build ':tomcat:7.0.54'
 
-        compile 'antlr:antlr:2.7.7'
-        compile 'net.sf.opencsv:opencsv:2.3'
-        compile "org.apache.lucene:lucene-core:2.4.0"
-        compile "org.apache.lucene:lucene-demos:2.4.0"
-        compile "org.apache.lucene:lucene-highlighter:2.4.0"
-        compile 'commons-net:commons-net:3.3' // used for ftp transfers
-        compile 'org.apache.commons:commons-math:2.2' //>2MB lib briefly used in ChartController
-        compile 'org.codehaus.groovy.modules.http-builder:http-builder:0.5.2', {
-            excludes 'groovy', 'nekohtml', 'httpclient', 'httpcore'
-        }
-        compile 'org.rosuda:Rserve:1.7.3'
-        compile 'com.google.guava:guava:18.0'
-        compile 'net.sf.ehcache:ehcache:2.9.0'
-        compile 'org.apache.httpcomponents:httpclient:4.4.1'
-        compile 'org.apache.httpcomponents:httpcore:4.4.1'
+		compile ':cache-ehcache:1.0.5'
+		compile ':codenarc:0.21'
+		compile ':hibernate:3.6.10.19'
+		compile ':quartz:1.0-RC2'
+		compile ':spring-security-core:2.0.0'
+		compile ':spring-security-kerberos:1.0.0'
+		compile ':spring-security-ldap:2.0.0'
+		compile ':spring-security-oauth2-provider:2.0-RC5'
 
-        compile 'org.apache.solr:solr-solrj:5.4.1'
-        compile 'org.apache.solr:solr-core:5.4.1'
+		runtime ':jquery-ui:1.10.4'
+		runtime ':jquery:1.11.1'
+		runtime ':prototype:1.0'
+		runtime ':resources:1.2.1'
 
-        /* we need at least servlet-api 2.4 because of HttpServletResponse::setCharacterEncoding */
-        compile "javax.servlet:servlet-api:$grails.servlet.version" /* delete from the WAR afterwards */
+		if (!dm) {
+			compile ':biomart-domain:16.2'
+			compile ':blend4j-plugin:16.2'
+			compile ':dalliance-plugin:16.2'
+			compile ':folder-management:16.2'
+			compile ':rdc-rmodules:16.2'
+			compile ':search-domain:16.2'
+			compile ':transmart-core:16.2'
+			compile ':transmart-gwas:16.2'
+			compile ':transmart-java:16.2'
+			compile ':transmart-legacy-db:16.2'
+			compile ':transmart-metacore-plugin:16.2'
+			compile ':transmart-mydas:16.2'
+			compile ':transmart-rest-api:16.2'
+			compile ':transmart-xnat-importer:16.2'
+			compile ':xnat-viewer:16.2'
 
-        /* for GeneGo web services: */
-        compile 'axis:axis:1.4'
-
-        /* for SAML authentication */
-        compile('org.springframework.security.extensions:spring-security-saml2-core:1.0.0.RELEASE') {
-            //excludes of spring securirty necessary because they are for an older version (3.1 branch)
-            //also remove xercesImpl because it breaks tomcat and is not otherwise needed
-            excludes 'spring-security-config', 'spring-security-core', 'spring-security-web', 'xercesImpl'
-        }
-        // spring security version should be in sync with that brought with
-        // grails-spring-security-core
-        runtime 'org.springframework.security:spring-security-config:3.2.3.RELEASE',
-                'org.springframework.security:spring-security-web:3.2.3.RELEASE', {
-            transitive = false
-        }
-
-        test('junit:junit:4.11') {
-            transitive = false /* don't bring hamcrest */
-            export = false
-        }
-
-        test 'org.hamcrest:hamcrest-core:1.3',
-                'org.hamcrest:hamcrest-library:1.3'
-
-        test 'org.gmock:gmock:0.9.0-r435-hyve2', {
-            transitive = false
-        }
-
-    }
-
-    plugins {
-        build ':release:3.1.1'
-        build ':tomcat:7.0.54'
-
-        compile ':hibernate:3.6.10.19'
-        compile ':rest-client-builder:2.1.1'
-        compile ':cache-ehcache:1.0.5'
-        compile ':quartz:1.0-RC2'
-        compile ':spring-security-kerberos:1.0.0'
-        compile ':spring-security-ldap:2.0-RC2'
-        compile ':spring-security-core:2.0-RC5'
-        compile ':spring-security-oauth2-provider:2.0-RC5'
-
-        runtime ':prototype:1.0'
-        runtime ':jquery:1.11.1'
-        runtime ':jquery-ui:1.10.4'
-        runtime ':resources:1.2.1'
-
-        // support for static code analysis - see codenarc.reports property below
-        compile ":codenarc:0.21"
-
-        if (!dm) {
-            compile ':rdc-rmodules:16.2-SNAPSHOT'
-            runtime ':transmart-core:16.2-SNAPSHOT'
-            compile ':transmart-gwas:16.2-SNAPSHOT'
-            //// already included in transmart-gwas
-            //compile ':transmart-legacy-db:16.2-SNAPSHOT'
-            //// already included in transmart-gwas
-            //compile ':folder-management:16.2-SNAPSHOT'
-            //// already included in transmart-gwas, folder-management
-            //compile ':search-domain:16.2-SNAPSHOT'
-            //// already included in search-domain, transmart-gwas,
-            //                       folder-management
-            //compile ':biomart-domain:16.2-SNAPSHOT'
-            //// already included in biomart-domain
-            //compile ':transmart-java:16.2-SNAPSHOT'
-            runtime ':dalliance-plugin:16.2-SNAPSHOT'
-            runtime ':transmart-mydas:16.2-SNAPSHOT'
-            runtime ':transmart-rest-api:16.2-SNAPSHOT'
-            runtime ':blend4j-plugin:16.2-SNAPSHOT'
-            runtime ':transmart-metacore-plugin:16.2-SNAPSHOT'
-            runtime ':transmart-xnat-importer:16.2-SNAPSHOT'
-            runtime ':xnat-viewer:16.2-SNAPSHOT'
-
-            test ':transmart-core-db-tests:16.2-SNAPSHOT'
-        } else {
-            dm.internalDependencies delegate
-        }
-
-        // Doesn't work with forked tests yet
-        //test ":code-coverage:1.2.6"
-    }
+			//test ":code-coverage:1.2.6" // Doesn't work with forked tests yet
+			test ':transmart-core-db-tests:16.2'
+		}
+		else {
+			dm.internalDependencies delegate
+		}
+	}
 }
 
 dm?.with {
-    configureInternalPlugin 'compile', 'rdc-rmodules'
-    configureInternalPlugin 'runtime', 'transmart-core'
-    configureInternalPlugin 'test', 'transmart-core-db-tests'
-    configureInternalPlugin 'compile', 'transmart-gwas'
-    configureInternalPlugin 'compile', 'transmart-java'
-    configureInternalPlugin 'compile', 'biomart-domain'
-    configureInternalPlugin 'compile', 'search-domain'
-    configureInternalPlugin 'compile', 'folder-management'
-    configureInternalPlugin 'compile', 'transmart-legacy-db'
-    configureInternalPlugin 'runtime', 'dalliance-plugin'
-    configureInternalPlugin 'runtime', 'transmart-mydas'
-    configureInternalPlugin 'runtime', 'transmart-rest-api'
-    configureInternalPlugin 'runtime', 'blend4j-plugin'
-    configureInternalPlugin 'runtime', 'transmart-metacore-plugin'
+	configureInternalPlugin 'compile', 'rdc-rmodules'
+	configureInternalPlugin 'runtime', 'transmart-core'
+	configureInternalPlugin 'test',    'transmart-core-db-tests'
+	configureInternalPlugin 'compile', 'transmart-gwas'
+	configureInternalPlugin 'compile', 'transmart-java'
+	configureInternalPlugin 'compile', 'biomart-domain'
+	configureInternalPlugin 'compile', 'search-domain'
+	configureInternalPlugin 'compile', 'folder-management'
+	configureInternalPlugin 'compile', 'transmart-legacy-db'
+	configureInternalPlugin 'runtime', 'dalliance-plugin'
+	configureInternalPlugin 'runtime', 'transmart-mydas'
+	configureInternalPlugin 'runtime', 'transmart-rest-api'
+	configureInternalPlugin 'runtime', 'blend4j-plugin'
+	configureInternalPlugin 'runtime', 'transmart-metacore-plugin'
 }
 
 dm?.inlineInternalDependencies grails, grailsSettings
 
-grails.war.resources = { stagingDir ->
-    delete(file: "${stagingDir}/WEB-INF/lib/servlet-api-${grails.servlet.version}.jar")
-}
-
 // Use new NIO connector in order to support sendfile
 // This is a lovely thought, but with Tomcat running Grails 2.3.6+ NIO does not function in run-war mode
 // Official bug number : GRAILS-11376
-if (!grails.util.Environment.isWarDeployed()) {
-    grails.tomcat.nio = true
+if (!Environment.isWarDeployed()) {
+	grails.tomcat.nio = true
 }
 
 codenarc.reports = {
-    TransmartAppReport('html') {
-        outputFile = 'CodeNarc-transmartApp-Report.html'
-        title = 'transmartApp Report'
-    }
+	TransmartAppReport('html') {
+		outputFile = 'CodeNarc-transmartApp-Report.html'
+		title = 'transmartApp Report'
+	}
 }
 
 // vim: set et ts=4 sw=4:
